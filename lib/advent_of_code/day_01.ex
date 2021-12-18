5530993739ac41adaf44e0fe0784e6ac1f2c0c93
Nicolas Cavigneaux <nico@bounga.org>
2021-12-19 00:29:57 +0100

    Day 01 part 01

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,5 +1,20 @@
 defmodule AdventOfCode.Day01 do
-  def part1(args) do
+  def part1(_args) do
+    input =
+      [
+        199,
+        200,
+        208,
+        210,
+        200,
+        207,
+        240,
+        269,
+        260,
+        263
+      ]
+      |> Enum.chunk_every(2, 1, :discard)
+      |> Enum.count(fn [left, right] -> right > left end)
   end
 
   def part2(args) do
